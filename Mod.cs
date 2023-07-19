534076c6027f9cdfdb0beafad5340c360933beec
adon <86583577+shg166@users.noreply.github.com>
2023-07-19 10:10:40 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_RoadNameList
+{
+	public class JP_RoadNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Road Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、バニラの道路名を変更します"; }
+		}
+
+	}
+}
