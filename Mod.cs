4784d1be9c2e56442f71385b314cc67270a7b8a4
adon <86583577+shg166@users.noreply.github.com>
2023-07-29 12:55:58 +0900

    Update V1.1
    General Streets
    - Add more street names with unique or generic names
    - Eliminated street names with vanilla names
    Bridges and Elevated
    - Add unique names

--- Mod.cs
+++ Mod.cs
@@ -11,7 +11,7 @@ namespace JP_RoadNameList
 
 		public string Name
 		{
-			get { return "JP Road Name List (CNL) v1.0"; }
+			get { return "JP Road Name List (CNL) v1.1"; }
 		}
 
 		public string Description
